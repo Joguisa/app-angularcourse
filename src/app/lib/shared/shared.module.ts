88dbd63c91e44483d814d1d550826269930df242
jguillen <jonatanguillen@sismetic.com>
2024-03-01 12:47:53 -0500

    error corrections when building the project, storing the logos in their respective folder.

--- src/app/lib/shared/shared.module.ts
+++ src/app/lib/shared/shared.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 const modules = [
   CommonModule,
   ReactiveFormsModule,
   FormsModule,
+  HttpClientModule,
 ];
 
 @NgModule({
-  // imports: [...modules],
-  // exports: [...modules],
+  imports: [...modules],
+  exports: [...modules],
 })
 export class SharedModule { }
